2092bc7d0c74f245af53a85ba1787804865ee299
Patience Mukashyaka <c-mukasp@sumner-log2.sumner.jax.org>
2022-12-13 20:42:58 -0500

    updated stitch well code

--- scripts/process_organoids/stitch_well.py
+++ scripts/process_organoids/stitch_well.py
@@ -10,14 +10,19 @@ from numcodecs import Blosc
 from pathlib import Path
 from skimage import io, morphology, filters, measure
 import pandas as pd
+import matplotlib
+matplotlib.use ('pdf')
+import matplotlib.pyplot as plt
+from skimage.morphology import square,binary_closing,dilation
+import cv2
 
 #global variable definition(all upper case variables)
+#layout of 25 fields, change to the appropriate layout
 LAYOUT_25 = np.array([[ 2, 3, 4, 5, 6],
                       [11,10, 9, 8, 7],
                       [12,13, 1,14,15],
                       [20,19,18,17,16],
                       [21,22,23,24,25]])
-
 
 # function definitions
 def parse_index_file(index_file_path, well_row, well_col):
@@ -132,14 +137,26 @@ def segment_image(image):
                                                          dtype=int))
     clean_img = img * processed[np.newaxis, np.newaxis, :, :]
     image_grey = (clean_img[0, ...] * 0.2125) + (clean_img[1, ...] * 0.7154)
-    image_2um_blur = filters.gaussian(image_grey, sigma=0.5, mode = 'reflect',
+    image_blur = filters.gaussian(image_grey, sigma=0.5, mode = 'reflect',
                                       multichannel=True , preserve_range=True)
-    thresh_value = filters.threshold_triangle(image_2um_blur.flatten())
-    mask_triangle_2um = image_2um_blur > thresh_value
-    seg_image = morphology.remove_small_objects(mask_triangle_2um,
-                                                min_size=8000)
-    return seg_image
+    thresh_value = filters.threshold_triangle(image_blur.flatten())
+    mask_triangle = image_blur > thresh_value
+    
+    return mask_triangle
 
+def calc_area(image):
+    segments = image.max(axis=0)
+    image_nbig = np.copy(segments)
+    component_sizes = np.bincount(segments.ravel())
+    too_big = component_sizes > 100000
+    too_big_mask = too_big[segments]
+    image_nbig[too_big_mask] = 0
+    images_nbig_dilated_300 = dilation(image_nbig, square(300))
+    image_nbig_dilated_closed_200 = binary_closing(images_nbig_dilated_300,
+                                                   selem=square(200), out=None)
+    image_area = cv2.countNonZero(image_nbig_dilated_closed_200.astype('int'))
+
+    return image_nbig_dilated_closed_200, image_area
 
 def parse_config(config_path):
     parser = configparser.ConfigParser()
@@ -155,6 +172,47 @@ def parse_config(config_path):
             plate_path,
             output_path)
 
+def plot_labeled_segments(array3d, fix_overlay=True, annotate=True):
+    '''
+    function for plotting z-projection of labeled segmentations.
+
+    arguments:
+        array3d (np.ndarray): input 3D array of labels
+        fix_overlay (bool): flag to decide how to treat segments which overlap 
+            along z-axis. If this is set to True, the overlapping organoids will
+            be shown with a different color. If flag is False, maximum of organoid labels
+            along z-axis is presented. (default: True)
+    '''
+    plt.figure(figsize=(10,10));
+    cmap = matplotlib.colors.ListedColormap(np.random.rand(256,3))
+
+    if fix_overlay:
+        tmp = 2**array3d
+        tmp.sort(axis=2)
+        tmp = tmp[:,:,0] + np.diff(tmp, axis=2).sum(axis=2)
+        plt.imshow(np.log2(tmp), cmap=cmap)
+    else:
+        tmp = array3d.max(axis=0)
+        plt.imshow(tmp, cmap=cmap)
+
+    if annotate:
+        for props in measure.regionprops(tmp):
+            y, x = props.centroid
+            if fix_overlay:
+                label = np.log2(props.label)
+            else:
+                label = props.label
+            if label == int(label):
+                label = int(label)
+            if label>0:
+                plt.text(x, y, label, fontsize=8, color='k', horizontalalignment='center', verticalalignment='center')
+
+def get_channels(orig_zarr):
+    new_zarr = np.zeros([101,5080,5080,3])
+    new_zarr[:,:,:,1] = orig_zarr[:,:,:,0] /orig_zarr[:,:,:,0].max()
+    new_zarr[:,:,:,0] = orig_zarr[:,:,:,1] /orig_zarr[:,:,:,1].max()
+
+    return new_zarr
 
 # script 
 def main(well_row, well_col, config_file):
@@ -174,7 +232,6 @@ def main(well_row, well_col, config_file):
                                                 overlap=(overlap_y,
                                                          overlap_x),
                                                 plane_size=plane_size)
-
 
     for f in range(1, nfield + 1):
         print(f'Processing field: {f}')
@@ -200,13 +257,61 @@ def main(well_row, well_col, config_file):
         zarr_con[:, :, rowstart:rowstart+plane_size[0], colstart:colstart+plane_size[1]] = orig_pixel_array
 
     #label segments and save rois
-    label_segment = measure.label(segment_con)
-    df = pd.DataFrame(measure.regionprops_table(label_segment, properties=('label','bbox', 
-                                                                    'area','major_axis_length',
-                                                                    'minor_axis_length'))).set_index('label')
-    df.to_csv(Path(output_path) / f"r{well_row:02}c{well_col:02}organoids.csv")
-    
+    label_segment = measure.label(segment_con, connectivity=3)
+    label_segment = morphology.remove_small_objects(label_segment,connectivity=3,
+                                            min_size=3000)
 
+    #reorder organoid labels
+    objects=np.ravel(label_segment)
+    objects=objects[objects>0]
+    labs=list(set(list(objects)))
+    label_segment_order=np.zeros(np.shape(label_segment), dtype=np.int32)
+    for i in range(len(labs)):
+        label_segment_order[label_segment==labs[i]]=i+1
+
+    #save lebeled organoids array as zarr
+    zarr.save(Path(output_path) / f"r{well_row:02}c{well_col:02}organoids.zarr",label_segment_order)
+  
+    #save organoid labeled image
+    plt.figure(figsize=(10,17))
+    plt.imshow(label_segment_order.max(axis=0), cmap =matplotlib.colors.ListedColormap(np.random.rand(256,3)))
+    plt.savefig(Path(output_path) / f"r{well_row:02}c{well_col:02}organoids.pdf")
+
+    #save organoid labeled image with label numbers on top 
+    plot_labeled_segments(label_segment_order,fix_overlay=False, annotate=True)
+    plt.savefig(Path(output_path) / f"r{well_row:02}c{well_col:02}organoidsN.pdf")
+
+    # calculate well area
+    image_area, area = calc_area(label_segment_order)
+    #save image area plot
+    plt.figure(figsize=(10,10))
+    plt.imshow(image_area)
+    plt.savefig(Path(output_path) / f"r{well_row:02}c{well_col:02}area.pdf")
+
+    #reorder axis of intensity image
+    image = zarr_con[:2, :, :, :]
+    image = np.moveaxis(image, 0, -1)
+
+    # measure features of organoids
+    df = pd.DataFrame(measure.regionprops_table(label_segment_order, intensity_image=image,
+                                                            properties=("label","centroid","bbox", 
+                                                                    "area", "major_axis_length",
+                                                                    "area_bbox",
+                                                                    "extent", "area_filled","area_convex", 
+                                                                    "euler_number","extent", 
+                                                                    "intensity_max","intensity_mean","intensity_min",
+                                                                    "inertia_tensor_eigvals",
+                                                                    "solidity"))).set_index('label') 
+
+    df['well_area'] = area
+    df.to_csv(Path(output_path) / f"r{well_row:02}c{well_col:02}organoids.csv")
+
+    #plot the original image
+    new_zarr = get_channels(image)
+    plt.figure(figsize=(10,10))
+    plt.imshow((new_zarr.max(axis=0))*3)
+    plt.savefig(Path(output_path) / f"r{well_row:02}c{well_col:02}cells.pdf")
+    
 if __name__ == "__main__":
     desc = ("Stitch all of the fields from the well of an Opera Phenix plate and"
             " output regions of interest.")
